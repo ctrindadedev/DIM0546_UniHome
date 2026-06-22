0553e17b26088bfab7bb4c1471608a855dd0072f
Caio Trindade <caiomedtrindade@gmail.com>
2026-06-21 21:04:57 -0300

    adiciona conexao do front com o back

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,10 @@
 const { Router } = require('express')
 const router = Router()
 
+router.get('/ping', (_req, res) => {
+    res.status(200).json({ success: true, message: 'pong' })
+})
+
 router.use('/properties', require('../modules/properties/property.route'))
 
 module.exports = router
